d9df1462c48199211d9901a42bb0fcca12e27577
krajco <patokrajc@gmail.com>
2016-12-03 20:47:31 +0100

    Add files via upload

---
+++ scanner.c
@@ -0,0 +1,282 @@
+#include "scanner.h"
+#include "err.h"
+
+int init_string(string *s){
+	s->str = (char *)malloc(8*sizeof(char));
+	if(s->str == NULL)
+		return ERR_INTERNAL_ERR;
+	s->length = 0;
+	s->alloc  = 8;
+	s->str[0] = '\0';
+	return TRUE;
+}
+
+int add_char_to_String(string *s,char c){
+	s->length++;
+	if(s->length >= s->alloc){
+		s->alloc++;
+		s->str = (char *)realloc(s->str,s->alloc * sizeof(char));
+		if(s->str == NULL)
+			return ERR_INTERNAL_ERR;
+	}
+	s->str[s->length-1] = c;
+	s->str[s->length] = '\0';
+	return TRUE;
+}
+
+void free_string(string *s){
+	for(unsigned int i = 0; i < (s->length-1); i++){
+		s->str[i] = '\0';
+	}
+	s->length = 0;
+	s->alloc  = 0;
+	free(s->str);
+}
+
+int compare_keywords(string *s){
+	if(!strcmp(s->str,"boolean"))
+		return kw_true;
+	if(!strcmp(s->str,"break"))
+		return kw_break;
+	if(!strcmp(s->str,"class"))
+		return kw_class;
+	if(!strcmp(s->str,"continue"))
+		return kw_continue;
+	if(!strcmp(s->str,"do"))
+		return kw_do;
+	if(!strcmp(s->str,"double"))
+		return kw_double;
+	if(!strcmp(s->str,"else"))
+		return kw_else;
+	if(!strcmp(s->str,"false"))
+		return kw_false;
+	if(!strcmp(s->str,"for"))
+		return kw_for;
+	if(!strcmp(s->str,"if"))
+		return kw_if;
+	if(!strcmp(s->str,"int"))
+		return kw_int;
+	if(!strcmp(s->str,"return"))
+		return kw_return;
+	if(!strcmp(s->str,"String"))
+		return kw_string;
+	if(!strcmp(s->str,"static"))
+		return kw_static;
+	if(!strcmp(s->str,"true"))
+		return kw_true;
+	if(!strcmp(s->str,"void"))
+		return kw_void;
+	if(!strcmp(s->str,"while"))
+		return kw_while;
+	return FALSE;
+}
+
+int get_token(FILE *f,string *str){
+	int state =  State_for_start;
+	int c;
+	int returnVal=1;
+	int read = TRUE;
+	free_string(str);
+	init_string(str);
+	while(read){
+		c = fgetc(f);
+		switch(state){
+			case State_for_start:
+				if(isspace(c)){}
+				else if(isalpha(c)){
+					add_char_to_String(str,c);
+					state = State_for_kw;
+				}
+				else if(c == '_' || c == '$'){
+					add_char_to_String(str,c);
+					state = State_for_id;
+				}
+				else{
+					switch(c){
+						printf("Switch 1\n");
+						case EOF:	read = FALSE; return EOF;
+						case '/':	state = char_slash;	break;
+						case '{':	read = FALSE;	returnVal = char_LMZatvorka;	break;
+						case '}': 	read = FALSE;	returnVal = char_PMZatvorka;	break;
+						case '(':	read = FALSE;	returnVal = char_LZatvorka;		break;
+						case ')':	read = FALSE;	returnVal = char_PZatvorka;		break;
+						case '[':	read = FALSE;	returnVal = char_LHZatvorka;	break;
+						case ']':	read = FALSE;	returnVal = char_PHZatvorka;	break;
+						case '.': 	read = FALSE; 	returnVal = char_bodka; 		break;
+        			    case ',': 	read = FALSE; 	returnVal = char_ciarka;		break;
+       				    case '+': 	read = FALSE; 	returnVal = char_plus; 			break;
+         			  	case '-': 	read = FALSE; 	returnVal = char_minus;			break;
+						case '*': 	read = FALSE; 	returnVal = char_star;			break;
+						case ':':	read = FALSE;	returnVal = char_dvoj_bodka;	break;
+						case ';':	read = FALSE;	returnVal = char_bod_ciarka;	break;
+						case '%':	read = FALSE;	returnVal = char_percento;		break;
+						case '\'':	read = FALSE;	returnVal = char_apostrof;		break;
+						case '\"':	read = FALSE;	returnVal = char_uvodzovky;		break;
+						case '<':	read = TRUE;	state = char_mensi;				break;
+						case '>':	read = TRUE;	state = char_vacsi;				break;
+						case '=':	read = TRUE;	state = char_rovnasa;			break;
+						case '!':	read = TRUE;	state = char_vykricnik;			break;
+						case '|':	read = TRUE;	state = char_pipe;				break;
+						case '&':	read = TRUE;	state = char_amperesand;		break;
+						case '\\':	read = TRUE;	state = char_backslash;			break;
+						default :	read = FALSE;	returnVal =	ERR_LEX_ERR;		break;
+					}
+					break;
+				}
+			case char_slash:
+				if(c == '/'){
+					returnVal = komentar1;
+				}else if(c == '*'){
+					returnVal = komentar2;
+				}else if(returnVal != komentar2 && returnVal != komentar1){
+					state = State_for_start;
+					returnVal = char_slash;
+					ungetc(c,f);
+				}
+
+				if(returnVal == komentar1){
+					c = fgetc(f);
+					if(c == '\n'){
+						state = State_for_start;
+					}
+					if(c == EOF){
+						returnVal = State_for_start;
+					}
+				}
+				if(returnVal == komentar2 && c == '*'){
+					if((c = fgetc(f)) == '/'){
+						state = State_for_start;
+					}
+					if(c == EOF){
+						read = FALSE;
+						returnVal = ERR_SYNTAX_ERR;
+					}
+				}
+				break;
+
+			case char_mensi :
+				if( c == '='){
+					read = FALSE;
+					returnVal = char_mensirovny;
+				}else{
+					read = FALSE;
+					returnVal = char_mensi;
+					ungetc(c,f);
+				}
+				break;
+
+			case char_vacsi :
+				if(c == '='){
+					read = FALSE;
+					returnVal = char_vacsirovny;
+				}else{
+					read = FALSE;
+					returnVal = char_vacsi;
+					ungetc(c,f);
+				}
+				break;
+
+			case char_rovnasa :
+				if(c == '='){
+					read = FALSE;
+					returnVal = char_rovny;
+				}else{
+					read = FALSE;
+					ungetc(c,f);
+					returnVal = char_rovnasa;
+				}
+				break;
+
+			case char_vykricnik :
+				if(c == '='){
+					read = FALSE;
+					returnVal = char_nerovny;
+				}else{
+					read = FALSE;
+					returnVal = char_vykricnik;
+					ungetc(c,f);
+				}
+				break;
+			case char_pipe :
+				if(c == '|'){
+					returnVal = char_or;
+					read = FALSE;
+				}else{
+					read = FALSE;
+					ungetc(c,f);
+					returnVal = char_pipe;
+				}
+				break;
+			case char_amperesand :
+				if( c =='&'){
+					read = FALSE;
+					returnVal = char_and;
+				}else{
+					read = FALSE;
+					ungetc(c,f);
+					returnVal = char_amperesand;
+				}
+				break;
+
+			case char_backslash :
+				if(c == '\\'){
+					returnVal = char_backslash;
+					read = FALSE;
+				}
+				else if(c == 't'){
+					read = FALSE;
+					returnVal = char_tab;
+				}
+				else if(c == 'n'){
+					read = FALSE;
+					returnVal = char_line;
+				}
+				else if(c == '\"'){
+					read = FALSE;
+					returnVal = print_uvodzovky;
+				}
+				else if(c == '\''){
+					read = FALSE;
+					returnVal = print_apostrof;
+				}else{
+					read = FALSE;
+					ungetc(c,f);
+					returnVal = ERR_SYNTAX_ERR;
+				}
+				break;
+
+			case State_for_kw :
+				add_char_to_String(str,c);
+				if(isspace(c) || c == EOF ){
+					if(compare_keywords(str) != FALSE){
+						returnVal = compare_keywords(str);
+						read = FALSE;
+					}else{
+						returnVal = premenna;
+					}
+				}
+				break;
+			case State_for_id :
+
+				break;
+		}
+	}
+	return returnVal;
+}
+int main(void)
+{
+	FILE *f = fopen("test.txt","r");
+	string *s;
+
+	s = malloc(sizeof(string));
+	if(f == NULL || s == NULL)
+		return -1;
+	init_string(s);
+	int x;
+	while(x  != EOF	){
+		x = get_token(f,s);
+		printf("%i\n", x);
+		
+	}
+	return 0;
+}
---
+++ scanner.h
@@ -0,0 +1,139 @@
+#ifndef SCANNER_INCLUDED
+#define SCANNER_INCLUDED
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+#define FALSE 			0
+#define TRUE 			1
+
+#define char_LMZatvorka 12 // {
+#define char_PMZatvorka 11 // }
+
+#define char_LZatvorka  20 // (
+#define char_PZatvorka  21 // )
+
+#define char_LHZatvorka 30 // [
+#define char_PHZatvorka 31 // ]
+
+#define char_backslash  40 /* \ */ //--GOTO
+#define char_pipe		41 // |		 --GOTO
+#define char_apostrof	42 // '
+#define char_uvodzovky	43 // "
+#define char_dvoj_bodka	44 // :
+#define char_bod_ciarka	45 // ;
+#define char_ciarka 	46 // ,
+#define char_amperesand 47 // &
+#define char_star		50 // *
+#define char_plus		51 // +
+#define char_minus		52 // -
+#define char_slash      53 /* / */
+#define char_rovnasa	54 // =
+#define char_percento 	55 // %
+#define char_mensirovny 56 // <=
+#define char_vacsirovny 57 // >=
+#define char_nerovny 	58 // !=
+#define char_rovny      59 // ==
+
+#define char_mensi		60 // <
+#define char_vacsi		61 // >
+#define char_vykricnik	62 // !
+
+#define char_bodka 		70 // .
+#define char_podtr		71 // _
+#define char_dolar		72 // $
+#define char_and 		73 // &&
+#define char_or 		74 // ||
+
+#define komentar1		81 // 
+#define komentar2		82 /* */
+#define char_tab		83
+#define char_line		84
+#define print_apostrof	85
+#define print_uvodzovky 86
+/***************************************************
+****************************************************/
+
+#define kw_static		100
+
+#define kw_int			101
+#define kw_double		102
+#define kw_string		103
+#define kw_boolean		104
+#define kw_void			105
+
+#define kw_continue		110
+#define kw_do 			111
+#define kw_while		112
+#define kw_for			113
+#define kw_break		114
+
+#define kw_class		120
+
+#define kw_else			130
+#define kw_if			131
+
+#define kw_return		140
+
+#define kw_true			150
+#define kw_false		151
+
+#define buff_size 		256
+
+#define State_for_start	200
+#define State_for_word	201
+#define State_for_char	202
+#define State_for_kw	203
+#define State_for_id	204
+
+#define premenna		205
+
+typedef struct{
+	char *str;
+	unsigned int length;
+	unsigned int alloc;
+}string;
+/**
+ * @brief      { function_description }
+ *
+ * @param[in]  lex   The lex
+ *
+ * @return     { description_of_the_return_value }
+ */
+int init_string(string *s);
+/**
+ * @brief      Adds a character to string.
+ *
+ * @param      s     { parameter_description }
+ * @param[in]  c     { parameter_description }
+ *
+ * @return     { description_of_the_return_value }
+ */
+int add_char_to_String(string *s,char c);
+/**
+ * @brief      { function_description }
+ *
+ * @param      s     { parameter_description }
+ */
+void free_string(string *s);
+
+/**
+ * @brief      { function_description }
+ *
+ * @param      s     { parameter_description }
+ *
+ * @return     { description_of_the_return_value }
+ */
+int compare_keywords(string *s);
+/**
+ * @brief      Gets the token.
+ *
+ * @param      f     { parameter_description }
+ * @param      s     { parameter_description }
+ *
+ * @return     The token.
+ */
+int get_token(FILE *f, string *s);
+#endif
