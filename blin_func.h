7d88fc035a2a103352c92133383f193d177862b5
Sibopius <sibop16@gmail.com>
2016-12-08 20:12:10 +0100

    Add files via upload

---
+++ blin_func.h
@@ -0,0 +1,12 @@
+
+#ifndef BUILTINS_H
+#define BUILTINS_H
+
+int lenght(char *text);
+char* substr(char *str, int i, int n);
+char* sort(char text[]);
+
+int readInt();
+double readDouble();
+char *readString();
+#endif
